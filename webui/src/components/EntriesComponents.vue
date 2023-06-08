99aa279c5a249b5af8c3846db374e40b57da9d35
Misaka <misaka@nekoit.org>
2023-06-08 21:45:23 +0800

    update

--- webui/src/components/EntriesComponents.vue
+++ webui/src/components/EntriesComponents.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
-import { NList, NListItem, NThing, NCard, NButton, NSpace } from 'naive-ui';
+import { NList, NListItem, NThing, NCard, NButton, NSpace, NModal, NInput, NSelect, NDynamicInput } from 'naive-ui';
 import axios from 'axios';
 import { CONFIG } from '../config';
 import { useStore } from '@/stores/storage';
 import { useDialog } from 'naive-ui';
 import { ref, onMounted } from 'vue';
 import type { Ref } from 'vue';
-import type { Entry } from '@/types';
+import type { Entry, Exec } from '@/types';
 
 const store = useStore();
 const dialog = useDialog();
+
+const actionTypeOptions = [
+  {
+    label: "Exec",
+    value: "exec"
+  }
+]
+const triggerTypeOptions = [
+  {
+    label: "Cron",
+    value: "cron"
+  }
+]
+
+const showEditModal = ref(false);
+const editModalActionType = ref("");
+const editModalTriggerType = ref("");
+
+const editingEntry = ref({} as Entry)
 
 const entries: Ref<Entry[]> = ref([]);
 
@@ -34,12 +53,37 @@ const fetchEntries = async () => {
         })
       }
     )
-
 };
+
+const handleEditEntry = (entry: Entry) => {
+  editingEntry.value = entry;
+  showEditModal.value = true;
+  switch (typeof entry.action) {
+    case "object":
+      console.log(Object.entries(entry.action)[0][0].toLowerCase())
+      editModalActionType.value = Object.entries(entry.action)[0][0].toLowerCase();
+      break;
+  }
+}
+
+const handleActionTypeUpdate = (value: string) => {
+  switch (value) {
+    case "exec":
+      editingEntry.value.action = {} as Exec;
+  }
+}
+
+const handleTriggerTypeUpdate = (value: string) => {
+  switch (value) {
+    case "cron":
+      editingEntry.value.trigger = {} as Cron;
+  }
+}
 
 onMounted(() => {
   fetchEntries()
 })
+
 
 </script>
 
@@ -51,7 +95,7 @@ onMounted(() => {
           <h2>{{ entry.name?.toString() }}</h2>
         </template>
         <template #suffix>
-          <n-button type="info">Edit</n-button>
+          <n-button type="info" @click="handleEditEntry(entry)">Edit</n-button>
         </template>
         <n-space>
           <n-thing title="Action" :description="Object.entries(entry.action)[0][0]"></n-thing>
@@ -60,4 +104,43 @@ onMounted(() => {
       </n-list-item>
     </n-list>
   </n-card>
+  <n-modal v-model:show="showEditModal" title="Edit Entry" preset="card" style="width: auto;">
+    <n-space vertical>
+      <div>
+        Name:
+        <n-input v-model:value="editingEntry.name" label="Name"></n-input>
+      </div>
+      <div>
+        ActionType:
+        <n-select @update:value="handleActionTypeUpdate" v-model:value="editModalActionType" label="ActionType"
+          :options="actionTypeOptions">
+        </n-select>
+      </div>
+      <n-spce vertical v-if='editModalActionType == "exec"'>
+        <div>
+          Executable:
+          <n-input v-model:value="(editingEntry.action as Exec).Exec.executable" placeholer="Executable"></n-input>
+        </div>
+        <div>
+          Args:
+          <n-dynamic-input v-model:value="(editingEntry.action as Exec).Exec.args" placeholder="Args" :min="1" />
+        </div>
+        <div>
+          Env:
+          <n-dynamic-input v-model:value="(editingEntry.action as Exec).Exec.env" placeholder="Env" :min="1" />
+        </div>
+        <div>
+          WorkingDir:
+          <n-input v-model:value="(editingEntry.action as Exec).Exec.working_dir" placeholder="WorkingDir"></n-input>
+        </div>
+      </n-spce>
+      <div>
+        TriggerType:
+        <n-select @update:value="handleTriggerTypeUpdate" v-model:value="editModalTriggerType" label="TriggerType"
+          :options="triggerTypeOptions">
+        </n-select>
+      </div>
+      <n-button type="primary">Save</n-button>
+    </n-space>
+  </n-modal>
 </template>
--- webui/src/types.ts
+++ webui/src/types.ts
@@ -1,11 +1,27 @@
+export interface Exec {
+  Exec: {
+    env: { [key: string]: string } | null,
+    working_dir: string | null,
+    executable: string,
+    user: string | null,
+    args: string[],
+  }
+}
+
+export interface Timer {
+
+}
+
+export enum Option {
+  None = "None",
+}
+
 export interface Entry {
   id: number
   name: string | null
-  action: {}
+  action: Exec | Option
   env: string[] | null
   working_dir: string | null
   logger: {}
   trigger: {}
 }
-
-
